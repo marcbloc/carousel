d7b832dc3e977db59b06e17eea904281cc47dc26
marblo <marcinblochowiak@wp.pl>
2017-05-12 07:00:04 +0200

    3

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(function(){
 	var carouselList = $("#carousel ul");
 	setInterval(changeSlide, 3000);
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+function changeSlide() {
+	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 }
+
 function moveFirstSlide(){
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
